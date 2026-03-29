4981b7fc1791bba45179793e52487dc355b4d643
Joshua Tompkins <jntompkins95@gmail.com>
2026-03-28 18:52:35 -0700

    Update logo and link to Propeller Aero Dirtmate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Logo */}
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+
+        {/* Link to Dirtmate */}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit Dirtmate
         </a>
       </header>
     </div>
